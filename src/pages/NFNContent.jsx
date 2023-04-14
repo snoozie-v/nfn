76b33e2fc0b025f4edea109af41fa8d735695cd3
snoozie-v <gettingsleepyz@gmail.com>
2023-04-14 16:30:09 -0700

    changed video width

--- src/pages/NFNContent.jsx
+++ src/pages/NFNContent.jsx
@@ -4,11 +4,11 @@ export default function NFNContent() {
   return (
     <div className="bg-slate-800 font-mono flex justify-center py-8">
     <div>
-    <iframe width="405" height="720" src="https://www.youtube.com/embed/IhyJBb_DZI4" title="Millions will flock to Web3 in the next few years. #shorts #blockchain #money #vechain" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-    <iframe width="405" height="720" src="https://www.youtube.com/embed/lHGdHzM5EaQ" title="ETH hit $2,000 with Shanghai Hard Fork | BULLRUN INCOMING?! #shorts #ethereum #crypto" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-    <iframe width="405" height="720" src="https://www.youtube.com/embed/fFo3bZ_Oqbw" title="$30 BILLION needs better management strategy. #shorts #blockchain" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-    <iframe width="405" height="720" src="https://www.youtube.com/embed/y_gs2dlJfpc" title="Changing the way over 3,000,000,000 people game. #games #shorts #gaming" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-    <iframe width="405" height="720" src="https://www.youtube.com/embed/7bTb6pnugNA" title="Bitcoin back to $30,000 | What you need to know. #shorts #gold ##money" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <iframe width="600" height="720" src="https://www.youtube.com/embed/IhyJBb_DZI4" title="Millions will flock to Web3 in the next few years. #shorts #blockchain #money #vechain" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <iframe width="600" height="720" src="https://www.youtube.com/embed/lHGdHzM5EaQ" title="ETH hit $2,000 with Shanghai Hard Fork | BULLRUN INCOMING?! #shorts #ethereum #crypto" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <iframe width="600" height="720" src="https://www.youtube.com/embed/fFo3bZ_Oqbw" title="$30 BILLION needs better management strategy. #shorts #blockchain" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <iframe width="600" height="720" src="https://www.youtube.com/embed/y_gs2dlJfpc" title="Changing the way over 3,000,000,000 people game. #games #shorts #gaming" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <iframe width="600" height="720" src="https://www.youtube.com/embed/7bTb6pnugNA" title="Bitcoin back to $30,000 | What you need to know. #shorts #gold ##money" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
     </div>
   )
