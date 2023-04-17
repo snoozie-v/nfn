840f78690a32401140c8893b587f175889323856
snoozie-v <gettingsleepyz@gmail.com>
2023-04-17 13:56:38 -0700

    video change

--- src/pages/NFNContent.jsx
+++ src/pages/NFNContent.jsx
@@ -11,8 +11,9 @@ export default function NFNContent() {
 
       </a>
       <div className="flex-grow min-h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
-        <iframe className="w-full h-80" src="https://www.youtube.com/embed/ldh_PR_6pG8" title="The $dollar$ is dying. They are LYING!  #money #bitcoin #shorts" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-        <iframe className="w-full h-80" src="https://www.youtube.com/embed/IhyJBb_DZI4" title="Millions will flock to Web3 in the next few years. #shorts #blockchain #money #vechain" allowFullScreen />
+      <iframe className="w-full h-80" src="https://www.youtube.com/embed/CUtwomVoSfs" title="Bye Bye middleman, hello future!  KEEP MORE OF YOUR MONEY! #tech #money #shorts #blockchain" allowfullscreen></iframe>
+      <iframe className="w-full h-80" src="https://www.youtube.com/embed/Dn5zguf7FgE" title="Understanding the $1.3 TRILLION dollar industry. #shorts #technology #money #motivation #learning"  allowfullscreen></iframe>
+
         <iframe className="w-full h-80" src="https://www.youtube.com/embed/lHGdHzM5EaQ" title="ETH hit $2,000 with Shanghai Hard Fork | BULLRUN INCOMING?! #shorts #ethereum #crypto" allowFullScreen />
         <iframe className="w-full h-80" src="https://www.youtube.com/embed/fFo3bZ_Oqbw" title="$30 BILLION needs better management strategy. #shorts #blockchain" allowFullScreen />
         <iframe className="w-full h-80" src="https://www.youtube.com/embed/y_gs2dlJfpc" title="Changing the way over 3,000,000,000 people game. #games #shorts #gaming" allowFullScreen />
