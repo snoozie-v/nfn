36eed62f2e85d1c8fa661feaa15fc39b1f9ea52a
snoozie-v <gettingsleepyz@gmail.com>
2023-06-08 21:59:54 -0700

    discord links

--- src/pages/Discord.jsx
+++ src/pages/Discord.jsx
@@ -11,42 +11,44 @@ function Discord() {
           
           <div className="justify-center flex py-6 text-bold text-blue-50 text-2xl">Discord Directory</div>
           <div className='sm:grid sm:grid-cols-2 sm:gap-2'>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">Banner Boi<a href="https://discord.gg/hn2R8C7XUW" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">Bored Flamingo Flying Club<a href="https://discord.gg/nkVM5XbmPw" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">BlackVeMarket<a href="https://discord.gg/CfQy7k2QQH" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">Corgi Gang<a href="https://discord.gg/pCsrw4mp6z" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">ExoWorlds<a href="https://discord.gg/mWDyWmNtp3" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">Gangster Gorillaz<a href="https://discord.gg/BD9ueyGqRp" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">Goatz Club<a href="https://discord.gg/8bV55nrRyq" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">The Inka Empire<a href="https://discord.gg/bT7WUH3dBT" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">KickBack Koalas<a href="https://discord.gg/eVeQ2vpgku" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">Mad V Apes<a href="https://discord.gg/madvapesnft" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">Mino Mob<a href="https://discord.gg/qp8ew4NxzG" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">New Pigs Order<a href="https://discord.gg/63n7MQ5xfP" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">No-Nerds Inc<a href="https://discord.gg/hEw89HMJfc" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">Non Fungible Book Club<a href="https://discord.gg/GwJCaCYbAw" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">NonFungiBulls<a href="https://discord.gg/6BjRw9KyTB" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">Ukiyoe Warriors<a href="https://discord.gg/uGwcVWmP59" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">PsychoBeasts<a href="https://discord.gg/psychobeasts" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">Shark Gang<a href="https://discord.gg/sharkgang" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">Smuzzies<a href="https://discord.gg/UF9ZufMgkM" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">StonerPunks<a href="https://discord.gg/hw7J3NRc9e" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">Thugs Paradise P2E Game<a href="https://discord.gg/fb2EDdjctV" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">Tradze Town<a href="https://discord.gg/ApqgjXejyh" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">Vechain.Energy<a href="https://discord.gg/2zFpyvFsTV" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">VeCowboys<a href="https://discord.gg/SWBVwMvq9q" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">VFox Alliance<a href="https://discord.gg/PzZ2gfQwbQ" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">VeeParrots<a href="https://discord.gg/7yDjdXgke8" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">Venonymous<a href="https://discord.gg/EaMZX7hFev" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">VeSea Official<a href="https://discord.gg/vesea" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">VeTower<a href="https://discord.gg/mXZthzZQAn" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">Vexchange<a href="https://discord.gg/6gqAvkWg47" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">Virtual Flame Studios<a href="https://discord.gg/byAsnUTwfc" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">VPunks<a href="https://discord.gg/2Xr3uZFm6V" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">VVar Dogs<a href="https://discord.gg/vvardogs" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">World of V<a href="https://discord.gg/worldofv" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">YEET<a href="https://discord.gg/C6jramq5M8" className='hover:underline'></a></div></div>
-          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50">Zillionaire Zombies<a href="https://discord.gg/FjPYu24GmW" className='hover:underline'></a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/hn2R8C7XUW" className='hover:underline'>Banner Boi</a></div></div>
+          
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/CfQy7k2QQH" className='hover:underline'>BlackVeMarket</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/pCsrw4mp6z" className='hover:underline'>Corgi Gang</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/mWDyWmNtp3" className='hover:underline'>ExoWorld</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/BD9ueyGqRp" className='hover:underline'>Gangster Gorillaz</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/8bV55nrRyq" className='hover:underline'>Goatz Club</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/bT7WUH3dBT" className='hover:underline'>The Inka Empire</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/eVeQ2vpgku" className='hover:underline'>KickBack Koalas</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/madvapesnft" className='hover:underline'>Mad V Apes</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/qp8ew4NxzG" className='hover:underline'>Mino Mob</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/63n7MQ5xfP" className='hover:underline'>New Pigs Order</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/hEw89HMJfc" className='hover:underline'>No-Nerds Inc</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/GwJCaCYbAw" className='hover:underline'>Non Fungible Book Club</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/6BjRw9KyTB" className='hover:underline'>NonFungiBulls</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/uGwcVWmP59" className='hover:underline'>Ukiyoe Warriors</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/uUjctMETt4" className='hover:underline'>PsychoBeasts</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/sharkgang" className='hover:underline'>Shark Gang</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/UF9ZufMgkM" className='hover:underline'>Smuzzies</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/hw7J3NRc9e" className='hover:underline'>StonerPunks</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/fb2EDdjctV" className='hover:underline'>Thugs Paradise P2E Game</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/ApqgjXejyh" className='hover:underline'>Tradze Town</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/2zFpyvFsTV" className='hover:underline'>Vechain.Energy</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/SWBVwMvq9q" className='hover:underline'>VeCowboys</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/PzZ2gfQwbQ" className='hover:underline'>VFox Alliance</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/7yDjdXgke8" className='hover:underline'>VeeParrots</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/XfkqShUErd" className='hover:underline'>VeFaM</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/EaMZX7hFev" className='hover:underline'>Venonymous</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/vesea" className='hover:underline'>VeSea Official</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/mXZthzZQAn" className='hover:underline'>VeTower</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/6gqAvkWg47" className='hover:underline'>Vexchange</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/nf3EWJBKZH" className='hover:underline'>Virtual Flame Studios</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/2Xr3uZFm6V" className='hover:underline'>VPunks</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/zvs3RNPfhf" className='hover:underline'>VVar Dogs</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/vZEGHwtWuM" className='hover:underline'>World of Animals</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/worldofv" className='hover:underline'>World of V</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/C6jramq5M8" className='hover:underline'>YEET</a></div></div>
+          <div className='p-1 bg-slate-800 rounded-lg text-center'><div className="p-4 text-blue-50"><a href="https://discord.gg/FjPYu24GmW" className='hover:underline'>Zillionaire Zombies</a></div></div>
           </div>
         </div>
         <div className='h-10 bg-slate-800 w-full'></div>
